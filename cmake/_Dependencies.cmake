27063e9a0cd7713b6848581ee31d87321635e89d
Nat! <nat@codeon.de>
2018-07-23 18:07:42 +0200

    updated so that no-require libraries don't produce fatal errors on linkage

--- cmake/_Dependencies.cmake
+++ cmake/_Dependencies.cmake
@@ -103,7 +103,7 @@ if( NOT MULLE_OBJC_EXPAT_FOUNDATION_LIBRARY)
          endif()
       endforeach()
    else()
-      message( FATAL_ERROR "MULLE_OBJC_EXPAT_FOUNDATION_LIBRARY was not found")
+      message( STATUS "MULLE_OBJC_EXPAT_FOUNDATION_LIBRARY is missing but it is marked as \"no-require\"")
    endif()
 endif()
 
@@ -156,5 +156,44 @@ if( NOT MULLE_OBJC_KVCFOUNDATION_LIBRARY)
       endforeach()
    else()
       message( FATAL_ERROR "MULLE_OBJC_KVCFOUNDATION_LIBRARY was not found")
+   endif()
+endif()
+
+
+if( NOT MULLE_OBJC_DECIMAL_FOUNDATION_LIBRARY)
+   find_library( MULLE_OBJC_DECIMAL_FOUNDATION_LIBRARY NAMES ${CMAKE_STATIC_LIBRARY_PREFIX}MulleObjCDecimalFoundation${CMAKE_STATIC_LIBRARY_SUFFIX} MulleObjCDecimalFoundation)
+   message( STATUS "MULLE_OBJC_DECIMAL_FOUNDATION_LIBRARY is ${MULLE_OBJC_DECIMAL_FOUNDATION_LIBRARY}")
+
+   # the order looks ascending, but due to the way this file is read
+   # it ends up being descending, which is what we need
+   if( MULLE_OBJC_DECIMAL_FOUNDATION_LIBRARY)
+      set( DEPENDENCY_LIBRARIES
+         ${DEPENDENCY_LIBRARIES}
+         ${MULLE_OBJC_DECIMAL_FOUNDATION_LIBRARY}
+         CACHE INTERNAL "need to cache this"
+      )
+      # temporarily expand CMAKE_MODULE_PATH
+      get_filename_component( _TMP_MULLE_OBJC_DECIMAL_FOUNDATION_ROOT "${MULLE_OBJC_DECIMAL_FOUNDATION_LIBRARY}" DIRECTORY)
+      get_filename_component( _TMP_MULLE_OBJC_DECIMAL_FOUNDATION_ROOT "${_TMP_MULLE_OBJC_DECIMAL_FOUNDATION_ROOT}" DIRECTORY)
+
+      # search for DependenciesAndLibraries.cmake to include
+      foreach( _TMP_MULLE_OBJC_DECIMAL_FOUNDATION_NAME in MulleObjCDecimalFoundation)
+         set( _TMP_MULLE_OBJC_DECIMAL_FOUNDATION_DIR "${_TMP_MULLE_OBJC_DECIMAL_FOUNDATION_ROOT}/include/${_TMP_MULLE_OBJC_DECIMAL_FOUNDATION_NAME}/cmake")
+         # use explicit path to avoid "surprises"
+         if( EXISTS "${_TMP_MULLE_OBJC_DECIMAL_FOUNDATION_DIR}/DependenciesAndLibraries.cmake")
+            unset( MULLE_OBJC_DECIMAL_FOUNDATION_DEFINITIONS)
+            list( INSERT CMAKE_MODULE_PATH 0 "${_TMP_MULLE_OBJC_DECIMAL_FOUNDATION_DIR}")
+            include( "${_TMP_MULLE_OBJC_DECIMAL_FOUNDATION_DIR}/DependenciesAndLibraries.cmake")
+            list( REMOVE_ITEM CMAKE_MODULE_PATH "${_TMP_MULLE_OBJC_DECIMAL_FOUNDATION_DIR}")
+            set( INHERITED_DEFINITIONS
+               ${INHERITED_DEFINITIONS}
+               ${MULLE_OBJC_DECIMAL_FOUNDATION_DEFINITIONS}
+               CACHE INTERNAL "need to cache this"
+            )
+            break()
+         endif()
+      endforeach()
+   else()
+      message( STATUS "MULLE_OBJC_DECIMAL_FOUNDATION_LIBRARY is missing but it is marked as \"no-require\"")
    endif()
 endif()
