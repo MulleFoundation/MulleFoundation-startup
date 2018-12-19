36fb839b7e921e5bb654203f2c2e07eb476852d5
Nat! <nat@codeon.de>
2018-12-19 17:32:32 +0100

    * modernized project structure

---
+++ src/mulle-foundation-startup.m
@@ -0,0 +1,13 @@
+//
+//  Foundation.m
+//  MulleFoundation
+//
+//  Created by Nat! on 04.04.16.
+//  Copyright © 2016 Mulle kybernetiK. All rights reserved.
+//
+#import "import.h"
+
+#import <MulleObjCStandardFoundation/private/mulle-foundation-universeconfiguration-private.h>
+#import "version.h"
+#import "mulle-foundation-startup-private.inc"
+
