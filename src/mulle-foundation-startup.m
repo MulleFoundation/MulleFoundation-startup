1484e0e959d08ae23796524ae7adbe18cd203f23
Nat! <nat@mulle-kybernetik.com>
2019-01-15 13:33:10 +0100

    fix startup code

--- src/mulle-foundation-startup.m
+++ src/mulle-foundation-startup.m
@@ -5,9 +5,23 @@
 //  Created by Nat! on 04.04.16.
 //  Copyright © 2016 Mulle kybernetiK. All rights reserved.
 //
+#define _GNU_SOURCE
+
 #import "import.h"
 
-#import <MulleObjCStandardFoundation/private/mulle-foundation-universeconfiguration-private.h>
 #import "version.h"
 #import "mulle-foundation-startup-private.inc"
 
+
+static void   bang( struct _mulle_objc_universe *universe,
+                    struct mulle_allocator *allocator,
+                    void *userinfo)
+{
+   struct _mulle_objc_universeconfiguration   config;
+
+   memcpy( &config,
+           mulle_objc_global_get_default_universeconfiguration(),
+           sizeof( config));
+
+   MulleFoundationBang( universe, allocator, &config);
+}
