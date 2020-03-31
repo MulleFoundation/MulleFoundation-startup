fb28f27993010f5bd8898d72a4afbf373407fd14
Nat! <nat@mulle-kybernetik.com>
2020-03-31 19:18:11 +0200

    add travis, update mulle-sde

--- cmake/share/StandaloneC.cmake
+++ cmake/share/StandaloneC.cmake
@@ -27,7 +27,7 @@ if( STANDALONE)
    endif()
 
    if( NOT STANDALONE_DEFINITIONS)
-      set( STANDALONE_DEFINITIONS ${MULLE_FOUNDATION_DEFINITIONS})
+      set( STANDALONE_DEFINITIONS ${mulle_foundation_DEFINITIONS})
    endif()
 
    #
